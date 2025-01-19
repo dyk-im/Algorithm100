6018a6378046453361a1a92a3752b9e148c35283
dyk <73778096+dyk-im@users.noreply.github.com>
2025-01-20 05:44:12 +0900

    [Silver V] Title: 수들의 합 5, Time: 244 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		int sum = 1;
+		int count = 1;
+        int start = 1;
+        int end = 1;
+
+        while(start < N){
+            
+            if(sum >= N){
+                if(sum == N){
+                    count++;
+                }
+                else{
+                    start++;
+                    end = start;
+                    sum = start;
+                }
+            }
+            end++;
+            sum += end;
+        }        
+		bw.write(count+"\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
