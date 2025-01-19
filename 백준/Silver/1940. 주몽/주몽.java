90b3e139b2f4513dc0dcfa830cce891f1f6d62b2
dyk <73778096+dyk-im@users.noreply.github.com>
2025-01-20 06:21:40 +0900

    [Silver IV] Title: 주몽, Time: 200 ms, Memory: 19960 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0;i<N; i++ ){
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		int start = 0;
+		int end = N-1;
+		int sum = 0;
+		int count = 0;
+		while(start < end){
+			sum = arr[start]+ arr[end];
+			if(sum != M){
+				if(sum > M)
+					end--;
+				else
+					start++;
+			}
+			if(sum == M){
+				count++;
+				start++;
+				end--;
+			}
+		}
+		bw.write(count+"\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
