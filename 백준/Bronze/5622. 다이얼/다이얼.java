43f17077cfbafa7555437acc460300bcffd400ae
dyk <73778096+dyk-im@users.noreply.github.com>
2025-02-12 04:28:08 +0900

    [Bronze II] Title: 다이얼, Time: 112 ms, Memory: 15784 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -3,13 +3,11 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		//StringTokenizer st = new StringTokenizer(br.readLine());
 
 		int sum = 0;
 		String number = br.readLine();
