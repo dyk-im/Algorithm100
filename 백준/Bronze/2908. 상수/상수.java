f3ea184c1baefefec24b14a6985e192df40d9983
dyk <73778096+dyk-im@users.noreply.github.com>
2025-02-12 03:50:10 +0900

    [Bronze II] Title: 상수, Time: 128 ms, Memory: 16220 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,20 +1,34 @@
-import java.util.Scanner;
- 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        
-		int A = in.nextInt();
-		int B = in.nextInt();
-        
-        in.close();
-        
-		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
-		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
-		
-		System.out.print(A > B ? A : B);
-	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		String A = st.nextToken();
+		String B = st.nextToken();
+		String revA = "";
+		String revB = "";
+		for (int i = 2; i >= 0; i--) {
+			revA = revA + A.charAt(i);
+		}
+		for (int i = 2; i >= 0; i--) {
+			revB = revB + B.charAt(i);
+		}
+		if(Integer.parseInt(revA) > Integer.parseInt(revB)) {
+			bw.write(revA + "\n");
+		}
+		if(Integer.parseInt(revA) < Integer.parseInt(revB)) {
+			bw.write(revB + "\n");
+		}
+		bw.flush();
+		br.close();
+		bw.close();
 	}
 }
