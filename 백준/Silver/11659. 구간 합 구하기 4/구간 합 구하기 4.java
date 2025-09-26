2eff2affb6c50ff406e9eb0f088fdfc2fbc050ba
dyk <73778096+dyk-im@users.noreply.github.com>
2025-09-26 23:17:02 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1240 ms, Memory: 63568 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,32 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
 
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[N+1]; int[] sumArr = new int[N+1];
-		sumArr[0]=0;
-		st = new StringTokenizer(br.readLine());
-		for(int idx = 1; idx <= N; idx++){
-			arr[idx]=Integer.parseInt(st.nextToken());
-			sumArr[idx]=sumArr[idx-1]+arr[idx];
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+		int[] sA = new int[N+1];
+		int[] aA = new int[N+1];
+		sA[0] = 0;
+
+		for(int i = 1; i < N+1; i++){
+			sA[i] = Integer.parseInt(st2.nextToken());
 		}
-		int i, j, answer = 0;
-		for(int idx = 0; idx < M; idx++) {
-			st = new StringTokenizer(br.readLine());
-			i = Integer.parseInt(st.nextToken());
-			j = Integer.parseInt(st.nextToken());
-			answer = sumArr[j] - sumArr[i - 1];
-			bw.write(Integer.toString(answer));
-			bw.newLine();
+
+		aA[0] = 0;
+		for(int i = 1; i<N+1; i++){
+			aA[i] = aA[i-1] + sA[i];
 		}
-		bw.flush();
+
+		int[] anList = new int[M];
+		for(int i=0; i<M; i++) {
+			StringTokenizer st3 = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st3.nextToken());
+			int y = Integer.parseInt(st3.nextToken());
+			anList[i] = aA[y]-aA[x-1];
+		}
+		for(int i = 0; i<M; i++){
+			System.out.println(anList[i]);
+		}
 		br.close();
-		bw.close();
 	}
 }
